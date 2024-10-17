c9523f2b78ae12816371e6e424f5ff5867a5385c
Muni <106088010+muni0112@users.noreply.github.com>
2024-10-17 11:45:54 +0000

    const 1

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,5 +1,11 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
+/nc function visualizarInformacoesGlobais(){
+    const res = await fetch(url)
+    const dados = await res.json()
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+}
 async function visualizarInformacoesGlobais() {
     const res = await fetch(url)
 
